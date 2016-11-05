32227c69ffdcde5cf58180d385d06829f0131f73
dkittle <don@kittle.ca>
2016-11-05 11:25:01 -0400

    moving some components to a separate object

--- src/main/scala/tutorial/Greeting.scala
+++ src/main/scala/tutorial/Greeting.scala
@@ -1,9 +1,12 @@
 package tutorial
 
+import org.scalajs.dom.{Event, XMLHttpRequest}
+
 import scala.util.Random
 import scala.scalajs.js
-import scala.scalajs.js.JSApp
+import scala.scalajs.js.{JSON, JSApp}
 import org.scalajs.jquery.{JQuery, jQuery}
+import tutorial.GreetingComponents._
 
 object Greeting extends JSApp {
 
@@ -12,16 +15,16 @@ object Greeting extends JSApp {
   }
 
   def setupUI(): Unit = {
-    jQuery("#greeting").append("Hello World")
+    jQuery(Heading).append("Hello World")
     genTable()
-    jQuery("#click-me-button").click(regenerateEmployees _)
+    jQuery(Button).click(regenerateEmployees _)
   }
 
   def genTable(): Unit = {
-    jQuery("#data").empty()
-    jQuery("#data").append(headerRow())
+    jQuery(Data).empty()
+    jQuery(Data).append(headerRow())
     User.genUsers().foreach { u =>
-      jQuery("#data").append(userRow(u))
+      jQuery(Data).append(userRow(u))
     }
   }
 
@@ -29,19 +32,6 @@ object Greeting extends JSApp {
     genTable()
   }
 
-  val SPAN = "<span></span>"
-  val SPAN_RIGHT = "<span class=\"floatright\"></span>"
-  val LIST_ITEM = "<li></li>"
-
-  def headerRow() = {
-    val username = jQuery(SPAN).text("Name")
-    val salary = jQuery(SPAN_RIGHT).text("Salary")
-
-    jQuery(LIST_ITEM)
-      .addClass("list-group-item tableheader")
-      .append(username)
-      .append(salary)
-  }
 
   def userRow(u: User) = {
     val username = jQuery(SPAN).text(s"${u.lastname}, ${u.firstname}")
@@ -59,10 +49,13 @@ case class User(firstname: String, lastname: String, salary: Double)
 
 object User {
 
+  def randomNameIndex = Random.nextInt(5) + 1
+  def randomSalary = (Random.nextInt(8) + 1) * 15000
+
   def genUsers() = {
     val firstnames = Random.shuffle(List("Dan", "Bob", "Debbie", "Minnie", "Steve", "Virindra"))
     val lastnames = Random.shuffle(List("Smith","LeBeef","Archer","Mango","Dreth", "Seth"))
-    for (i <- 0 to 5) yield User(firstnames(i), lastnames(i), (Random.nextInt(6) + 1) * 15000)
+    for (i <- 0 to 5) yield User(firstnames(randomNameIndex), lastnames(randomNameIndex), randomSalary)
   }
 
 }
